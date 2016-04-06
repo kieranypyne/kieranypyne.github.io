162317a0c2ebb71f732cb517f6cfa1bd14b46f39
kieranypyne <kieranypyne@gmail.com>
2016-04-06 15:17:06 -0500

    plz end my suffering

---
+++ AustinCodingAcademy_Spring2016/Week8/FinalProject/js/pausegif.js
@@ -0,0 +1,62 @@
+$(document).ready(function()
+{
+    $("#imgAnimate1").hover(
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image1.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image1.png");
+        });
+
+    $("#imgAnimate2").hover(
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image2.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image2.png");
+        });
+
+    $("#imgAnimate3").hover(
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image3.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image3.png");
+        });
+
+    $("#imgAnimate4").hover(
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image4.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image4.png");
+        });
+
+    $("#imgAnimate5").hover(
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image5.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image5.png");
+        });
+
+    $("#imgAnimate6").hover(
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image6.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "/AustinCodingAcademy_Spring2016/Week8/FinalProject/img/image6.png");
+        });
+});
