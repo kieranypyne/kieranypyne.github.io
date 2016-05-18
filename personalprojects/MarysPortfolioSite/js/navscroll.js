67da43c2d1d699a391913baeed9f295592f69859
kieranypyne <kieranypyne@gmail.com>
2016-05-17 23:27:26 -0500

    Mary's site test

---
+++ personalprojects/MarysPortfolioSite/js/navscroll.js
@@ -0,0 +1,20 @@
+// $(function() {
+//
+//     var $navbar   = $("#navscroll"),
+//         $window    = $(window),
+//         offset     = $navbar.offset(),
+//         topPadding = 0;
+//
+//     $window.scroll(function() {
+//         if ($window.scrollTop() > offset.top) {
+//             $navbar.stop().animate({
+//                 marginTop: $window.scrollTop() - offset.top + topPadding
+//             });
+//         } else {
+//             $navbar.stop().animate({
+//                 marginTop: 0
+//             });
+//         }
+//     });
+//
+// });
